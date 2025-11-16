5c558ee3fd902d7aa380019214ad72276c3b2361
Adam Ulichny <abrooksny@protonmail.com>
2025-11-15 19:09:35 -0600

    Added documentation to generic powerlaw module

--- src/dist/powerlaw.rs
+++ src/dist/powerlaw.rs
@@ -11,50 +11,78 @@
 //! common expressions such as the Pareto Type I pdf listed at:
 //! [https://en.wikipedia.org/wiki/Pareto_distribution](https://en.wikipedia.org/wiki/Pareto_distribution)
 use super::Distribution;
-use rand::prelude::*;
 
-/// Powerlaw struct with parameters alpha and x_min.
+/// Represents a generic Power-Law distribution, which is a continuous, unbounded distribution
+/// that simplifies to a Pareto Type I distribution.
+///
+/// # Fields
+/// - `alpha`: The scaling parameter (α) of the distribution. Must be greater than 1.
+/// - `x_min`: The minimum value of the distribution (x_m). Must be positive.
 pub struct Powerlaw {
     pub alpha: f64,
     pub x_min: f64,
 }
 
+/// Implements the `Distribution` trait for the `Powerlaw` distribution.
 impl Distribution for Powerlaw {
-    /// Continuous, Unbounded Power Law which simplifies to a Pareto Type I.
+    /// Calculates the probability density function (PDF) at a given point `x`.
+    ///
     /// The alpha parameter will be exactly 1.0 greater than that of more
-    /// common expressions such as the pdf listed at:
+    /// common expressions such as the PDF listed at:
     /// <https://en.wikipedia.org/wiki/Pareto_distribution>
+    ///
+    /// # Parameters
+    /// - `x`: The point at which to evaluate the PDF. Must be greater than or equal to `x_min`.
+    ///
+    /// # Returns
+    /// The PDF value at `x`.
     fn pdf(&self, x: f64) -> f64 {
         (self.alpha - 1.) / self.x_min.powf(1. - self.alpha) * x.powf(-self.alpha)
     }
 
+    /// Calculates the cumulative distribution function (CDF) value at a given point `x`.
+    ///
+    /// # Parameters
+    /// - `x`: The point at which to evaluate the CDF.
+    ///
+    /// # Returns
+    /// The CDF value at `x`.
     fn cdf(&self, x: f64) -> f64 {
-        /*
-        Continuous, Unbounded Power Law
-         */
         1. - (x / self.x_min).powf(-self.alpha + 1.)
     }
 
+    /// Calculates the complementary cumulative distribution function (CCDF)
+    /// (also known as the survival function) value at a given point `x`.
+    ///
+    /// # Parameters
+    /// - `x`: The point at which to evaluate the CCDF.
+    ///
+    /// # Returns
+    /// The CCDF value at `x`.
     fn ccdf(&self, x: f64) -> f64 {
         1. - self.cdf(x)
     }
 
+    /// Generates a random variate from the Power-Law distribution using the inverse transform method.
+    ///
+    /// # Parameters
+    /// - `u`: A random number drawn from a Uniform(0,1) distribution.
+    ///
+    /// # Returns
+    /// A random variate `x` from the Power-Law distribution.
     fn rv(&self, u: f64) -> f64 {
-        /*
-        Random variate generation via inverse transform of the Pareto Type I pdf
-
-        Parameters
-        ----------
-        u: f64
-            Random Uniform (0,1)
-
-         */
-
         self.x_min * (1. - u).powf(-1. / (self.alpha - 1.))
     }
 }
 
-/// MLE estimator for the alpha parameter.
+/// Calculates the Maximum Likelihood Estimate (MLE) for the alpha parameter of a power-law distribution.
+///
+/// # Parameters
+/// - `data`: A slice of `f64` values representing the data points. It's assumed that all `x` in `data` are `>= x_min`.
+/// - `x_min`: The minimum value of the distribution.
+///
+/// # Returns
+/// The estimated alpha parameter as an `f64`.
 pub fn alpha_hat(data: &[f64], x_min: f64) -> f64 {
     let n: usize = data.len();
 
